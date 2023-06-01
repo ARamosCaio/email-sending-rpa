cd648de4977c06f480570b1852fa2270b2aaa7ff
Caio Ramos <aramoscaio@gmail.com>
2023-06-01 19:27:23 -0300

    feat: added images to the email

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from email.message import EmailMessage
 import smtplib
+import imghdr
 
 email = 'caio.araujo.cvo@gmail.com'
 senha = input('Digite a senha do email: ')
@@ -9,6 +10,8 @@ msg['Subject'] = 'Email automático'
 msg['From'] = email
 msg['To'] = 'lety5000.amarante@gmail.com'
 msg.set_content('Teste de e-mail automático utilizando python')
+
+files = ['1.png', '2.png']
 
 with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
     smtp.login(email, senha)
